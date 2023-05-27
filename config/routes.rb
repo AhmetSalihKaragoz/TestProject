fbe0325867435e031189f8e6a159ef3c05691081
Ahmet Salih Karagöz <ahmetkaragozz3455@gmail.com>
2023-05-27 21:25:23 +0300

    sign up form added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
